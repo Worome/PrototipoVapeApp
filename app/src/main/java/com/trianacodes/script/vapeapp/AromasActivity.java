c6016d336e2406b47c316bfa769cd8b599237caa
Worome <41673409+Worome@users.noreply.github.com>
2018-07-27 13:45:44 +0200

    Corregidos los siguiente errores:
    * ControlaSb...
    * ControlEt...
    * Limitarel número de cifras en los EditText correspondientes a los SeekBar
    * Puesto en los EditText correspondientes a los SeekBar que el valor por defecto sea 0

--- app/src/main/java/com/trianacodes/script/vapeapp/AromasActivity.java
+++ app/src/main/java/com/trianacodes/script/vapeapp/AromasActivity.java
@@ -1,20 +1,35 @@
 package com.trianacodes.script.vapeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import static java.lang.Integer.parseInt;
 
+// TODO: Ver cómo mostrar el cursor en un EditText
+// TODO: Mejorar diseño Activity
+// TODO: Hacer que el primer foco sea sobre Nombre del aroma
+// TODO: Hacer diseño para tablets
+// TODO: Cambiar apariencia a los SeekBar
+// TODO: Hacer que al recibir el foco un EditText seleccione todo el texto
+// TODO: Ver cómo hacer que se escriba de izquierda a derecha en EditText
+/* Todo: Anotar en los apuntes de Android que para hacer que un componente de la interfaz no obtenga
+         nunca el foco, hay que hacerlo desde el XML de la interfaz, en la etiqueta del elemento,
+         poniendo android:focusable = "false"*/
+/* Todo: Anotar en los apuntes de Android que para controlar si un EditText está vacío o no, se usa
+         el método isEmpty()*/
 public class AromasActivity extends AppCompatActivity {
 
     private Spinner desplegable;
     private SeekBar sbPorcentaje, sbMinMaceracion, sbMaxMaceracion;
     private EditText ePorcentaje, eMinMaceracion, eMaxMaceracion;
+    private String controlVacio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +37,46 @@ public class AromasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aromas);
 
-
         sbPorcentaje = findViewById(R.id.sbPorcentaje);
         ePorcentaje = findViewById(R.id.etPorcentaje);
+        sbMinMaceracion = findViewById(R.id.sbMinMaceracion);
+        eMinMaceracion = findViewById(R.id.etMinMaceracion);
+        sbMaxMaceracion = findViewById(R.id.sbMaxMaceracion);
+        eMaxMaceracion = findViewById(R.id.etMaxMaceracion);
+        Procesos();
         //Llamo a la función que rellena de datos el desplegable
-        rellenaDesplegable();
+        //rellenaDesplegable();
         /*Llamo a la función que va a controlar si se cambia el valor del SeekBar y si es así,
         modificar el contenido del EditText correspondiente*/
-        controlaSbPorcetaje();
-        controlaSbMinimo();
-        controlaSbMaximo();
-        /*Llamo a la función que va a controlar si el contenido del EditText ha cambiado y si es
-        así, cambio el valor del SeekBar.*/
-        controlaEdtPorcentaje();
-        controlaEdtMinimo();
-        controlaEdtMaximo();
+        //controlaSbPorcetaje();
+        //controlaSbMinimo();
+        //controlaSbMaximo();
+
+
+    }
+
+    public void Procesos(){
+
+        try {
+
+            //Llamo a la función que rellena de datos el desplegable
+            rellenaDesplegable();
+            /*Llamo a la función que va a controlar si se cambia el valor del SeekBar y si es así,
+            modificar el contenido del EditText correspondiente*/
+            controlaSbPorcetaje();
+            controlaSbMinimo();
+            controlaSbMaximo();
+             /*Llamo a la función que va a controlar si el contenido del EditText ha cambiado y si es
+            así, cambio el valor del SeekBar.*/
+            controlaEdtPorcentaje();
+            controlaEdtMinimo();
+            controlaEdtMaximo();
+
+        } catch (Exception e) {
+
+            Toast.makeText(this, "Error " + e, Toast.LENGTH_SHORT);
+
+        }
 
     }
 
@@ -57,58 +97,76 @@ public class AromasActivity extends AppCompatActivity {
     }
 
     public void controlaSbPorcetaje(){
+        try {
 
-        sbPorcentaje.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                sbPorcentaje.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
-            @Override
-            public void onProgressChanged(SeekBar porcentaje, int i, boolean b) {
+                @Override
+                public void onProgressChanged(SeekBar porcentaje, int i, boolean b) {
 
-                /* He creado en string.XML la etiqueta Aromas_FormatoString para que el número
-                almacenado en porcentaje.getProgress() lo tome el EditText con formato texto, ya
-                que los EditText sólo admite contenido de tipo String.*/
-                ePorcentaje.setText(getString(R.string.Aromas_FormatoString,
-                        porcentaje.getProgress()));
+                /* Uso String.ValueOf para que el número almacenado en porcentaje.getProgress()
+                lo tome el EditText con formato texto, ya que los EditText sólo admiten contenido
+                de tipo String.*/
+                    //ePorcentaje.setText(getString(R.string.Aromas_FormatoString,
+                    //porcentaje.getProgress()));
+                    ePorcentaje.setText(String.valueOf(porcentaje.getProgress()));
 
-            }
+                }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
 
-            }
+                }
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
 
-            }
-        });
+                }
+            });
+
+        } catch (Exception e) {
+
+            Toast.makeText(this, "El error es: " + e, Toast.LENGTH_SHORT);
+
+        }
 
     }
 
     public void controlaSbMinimo(){
 
-        sbMinMaceracion.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar minimo, int i, boolean b) {
+        try {
 
-                 /* He creado en string.XML la etiqueta Aromas_FormatoString para que el número
-                almacenado en porcentaje.getProgress() lo tome el EditText con formato texto, ya
-                que los EditText sólo admite contenido de tipo String.*/
-                eMinMaceracion.setText(getString(R.string.Aromas_FormatoString,
-                        minimo.getProgress()));
+                sbMinMaceracion.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar minimo, int i, boolean b) {
 
-            }
+                 /* Uso String.ValueOf para que el número almacenado en porcentaje.getProgress()
+                lo tome el EditText con formato texto, ya que los EditText sólo admiten contenido
+                de tipo String.*/
+                    //eMinMaceracion.setText(getString(R.string.Aromas_FormatoString,
+                    //      minimo.getProgress()));
+                    eMinMaceracion.setText(String.valueOf(minimo.getProgress()));
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+                }
 
-            }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+                }
 
-            }
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
 
-        });
+                }
+
+            });
+
+
+        } catch (Exception e) {
+
+            Toast.makeText(this, "El error es: " + e, Toast.LENGTH_SHORT);
+
+        }
 
     }
 
@@ -118,12 +176,12 @@ public class AromasActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar maximo, int i, boolean b) {
 
-                 /* He creado en string.XML la etiqueta Aromas_FormatoString para que el número
-                almacenado en porcentaje.getProgress() lo tome el EditText con formato texto, ya
-                que los EditText sólo admite contenido de tipo String.*/
-                eMaxMaceracion.setText(getString(R.string.Aromas_FormatoString,
-                        maximo.getProgress()));
-
+                 /* Uso String.ValueOf para que el número almacenado en porcentaje.getProgress()
+                lo tome el EditText con formato texto, ya que los EditText sólo admiten contenido
+                de tipo String.*/
+                //eMaxMaceracion.setText(getString(R.string.Aromas_FormatoString,
+                  //      maximo.getProgress()));
+                eMaxMaceracion.setText(String.valueOf(maximo.getProgress()));
             }
 
             @Override
@@ -142,80 +200,116 @@ public class AromasActivity extends AppCompatActivity {
 
     public void controlaEdtPorcentaje(){
 
-        ePorcentaje.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        try {
 
-            }
+            ePorcentaje.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                }
 
-               // if (ePorcentaje.getText().toString() != null){
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                    sbPorcentaje.setProgress(parseInt(ePorcentaje.getText().toString()));
+                    controlVacio = ePorcentaje.getText().toString();
+                    if (controlVacio.isEmpty()){
 
-                //} else {
+                        controlVacio = "0";
 
-                //    sbPorcentaje.setProgress(0);
+                    }
+                    sbPorcentaje.setProgress(parseInt(controlVacio));
 
-                //}
+                }
 
-            }
+                @Override
+                public void afterTextChanged(Editable editable) {
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+                }
+            });
 
-            }
-        });
+        } catch (Exception e){
+
+            Toast.makeText(this, "El error es: " + e, Toast.LENGTH_SHORT);
+
+        }
 
     }
 
     public void controlaEdtMinimo(){
 
-        eMinMaceracion.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        try {
 
-            }
+            eMinMaceracion.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                }
 
-                sbMinMaceracion.setProgress(parseInt(eMinMaceracion.getText().toString()));
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            }
+                   controlVacio = eMinMaceracion.getText().toString();
+                    if (controlVacio.isEmpty()){
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+                        controlVacio = "0";
 
-            }
+                    }
 
-        });
+                    sbMinMaceracion.setProgress(parseInt(controlVacio));
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+
+            });
+
+
+        } catch (Exception e) {
+
+            Toast.makeText(this, "El error es: " + e, Toast.LENGTH_SHORT);
+
+        }
 
     }
 
     public void controlaEdtMaximo(){
 
-        eMaxMaceracion.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        try{
 
-            }
+            eMaxMaceracion.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                }
 
-                sbMaxMaceracion.setProgress(parseInt(eMaxMaceracion.getText().toString()));
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            }
+                    controlVacio = eMaxMaceracion.getText().toString();
+                    if (controlVacio.isEmpty()){
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+                        controlVacio = "0";
 
-            }
+                    }
+                    sbMaxMaceracion.setProgress(parseInt(controlVacio));
 
-        });
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+
+            });
+
+        } catch (Exception e){
+
+            Toast.makeText(this, "El error es: " + e, Toast.LENGTH_SHORT);
+
+        }
 
     }
 
