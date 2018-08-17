fc21008d6b41b0ce514500d7cac7ff01a908c33d
Worome <41673409+Worome@users.noreply.github.com>
2018-08-17 14:50:23 +0200

    Comenzando pantalla inicio 3

--- app/src/main/java/com/trianacodes/script/vapeapp/ui/Inicio.java
+++ app/src/main/java/com/trianacodes/script/vapeapp/ui/Inicio.java
@@ -1,15 +1,69 @@
 package com.trianacodes.script.vapeapp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import com.trianacodes.script.vapeapp.clases.Aromas;
 
 import com.trianacodes.script.vapeapp.R;
 
 public class Inicio extends AppCompatActivity {
 
+    public ImageButton calculadora, aromas, almacen, recetas, gastos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        calculadora = findViewById(R.id.ibCalculadora);
+        aromas = findViewById(R.id.ibAromas);
+        almacen = findViewById(R.id.ibAlmacen);
+        recetas = findViewById(R.id.ibRecetas);
+        gastos = findViewById(R.id.ibGastos);
+        calculadora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        });
+
+        aromas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent llamaAromas = new Intent(Inicio.this, Aromas.class);
+
+            }
+
+        });
+
+        almacen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        });
+
+        recetas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        });
+
+        gastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        });
+
     }
 }
