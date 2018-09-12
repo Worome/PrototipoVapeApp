ef87b2d28d6f1c60949034c645cae65f446d5ed3
Worome <41673409+Worome@users.noreply.github.com>
2018-09-12 13:30:59 +0200

    Cambiando botones menú

---
+++ app/src/main/java/com/trianacodes/script/vapeapp/ui/ListadoAromas.java
@@ -0,0 +1,15 @@
+package com.trianacodes.script.vapeapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.trianacodes.script.vapeapp.R;
+
+public class ListadoAromas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_aromas);
+    }
+}
